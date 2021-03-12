ff3d9b25d9d64322f4c692c6bdea26fd65f85871
Tamo Bee <tamo.nyc@gmail.com>
2021-03-12 02:29:28 -0500

    updating py file

--- SupplyChain_tntb.py
+++ SupplyChain_tntb.py
@@ -1,7 +1,29 @@
-from Crypto_tntb import convertDataToJSON, pinJSONtoIPFS, initContract, w3
+from Contract_tntb import convertDataToJSON, pinJSONtoIPFS, initContract, w3
 import sys
 
 wine_registry = initContract()
+
+def callRegisterWineBottle():
+    owner = input("Address: ")
+    name = input("Wine Name: ")
+    producer = input("Producer name: ")
+    initial_value = int(input("Price: "))
+    possessor = input("Possessor name: ")
+    token_uri = input("Token URI: ")
+
+    txn_hash = wine_registry.functions.registerWineBottle(owner, name, producer, initial_value, possessor, token_uri).transact({"from": w3.eth.accounts[0]})
+    txn_receipt = w3.eth.waitForTransactionReceipt(txn_hash)
+
+    return txn_receipt
+
+def callGetWineInfo():
+    token_id = int(input("Token id: "))
+    txn_hash = wine_registry.functions.getWineInfo(token_id).transact({"from": w3.eth.accounts[0]})
+    txn_receipt = w3.eth.waitForTransactionReceipt(txn_hash)
+    wine = wine_registry.functions.rare_wines(token_id).call()
+    owner_chain = wine_registry.functions.owner_chains(token_id, 0).call()
+
+    return print("Wine Info: ", wine[0:5], "Owner Chain: ", owner_chain[0:])
 
 def createAppraisalReport():
 
@@ -25,7 +47,7 @@ def callEventAppraisal(token_id):
         fromBlock="0x0", argument_filters={"token_id": token_id}
     )
     return appraisal_filter.get_all_entries()
-
+ 
 
 def main():
     if sys.argv[1] == "report":
@@ -34,6 +56,14 @@ def main():
 
         print(receipt)
         print("Report URI:", report_uri)
+
+    elif sys.argv[1] == "register_wine":
+        # owner, name, producer, initial_value, possessor, token_uri = callRegisterWineBottle()
+        registration_receipt = callRegisterWineBottle()
+        print(registration_receipt)
+    
+    elif sys.argv[1] == "info":
+        callGetWineInfo()
 
     elif sys.argv[1] == "get":
         token_id = int(sys.argv[2])
